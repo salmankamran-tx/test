ccf52ee0e6e4594ebb8037c6e997faa753afa425
salman kamran <salmankamran@techcreatix.com>
2019-07-03 13:23:19 +0500

    new commit through master branch

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('testing-branch', 'Controller@testing');
+Route::get('testing-branch', 'Controller@testing'); //  editing
