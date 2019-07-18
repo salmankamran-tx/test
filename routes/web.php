be6f0306736e4ceabf2fa8bae16c02e9df647f20
salman kamran <salmankamran@techcreatix.com>
2019-07-18 19:01:13 +0500

    local changes

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
+
 Route::get('testing-branch', 'Controller@testing'); //  editing
-=======
+
 Route::get('testing-branch', 'Controller@testing');
 
 Auth::routes();
@@ -27,4 +27,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'PostController@home');
 
 Route::resource('/posts', 'PostController');
->>>>>>> feature
